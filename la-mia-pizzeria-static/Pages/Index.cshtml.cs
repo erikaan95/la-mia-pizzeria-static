3e44a4aba0b5c541303a5805f0bd973a037f54f1
erikaan95 <ancoraerika@gmail.com>
2022-05-09 03:32:37 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace la_mia_pizzeria_static.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
